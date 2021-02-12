87f78e19f9a1ad213b2b89f641a5ffcf63fc4c81
Victor Moscone <victormoscone@gmail.com>
2021-02-11 18:09:17 -0800

    Feat: Added all 4 routes to the api.js page. Unfortunately, they inconsistently work and don't work and I don't know why.

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,30 @@ const db = require('../models/fitness');
 
 module.exports = (app) => {
 
+    // This will find all current Workouts within the workout collection.
     app.get('/api/workouts', (req, res) => {
       db.find({}).then((workout) => {
           res.send(workout)
       });
     });
 
-    // /api/workouts
+    app.get('/api/workouts/range', (req, res) => {
+        db.find({}).then((workout) => {
+            res.json(workout);
+        });
+    });
+
+    app.post('/api/workouts', (req, res) => {
+        db.create(req.body).then((data) => {
+            res.send(data);
+        });
+    });
+
+    app.put('/api/workouts/:id', (req, res) => {
+        let paramId = req.params.id;
+        db.findByIdAndUpdate(paramId, {$push: {exercises: req.body}}).then((data) => {
+            res.send(data);
+        })
+    });
 
 };
