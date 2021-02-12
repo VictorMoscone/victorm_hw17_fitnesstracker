ba92a8f4c9e5511eb186a6962c4cb1ce23eb4d23
Victor Moscone <victormoscone@gmail.com>
2021-02-11 16:01:55 -0800

    Feat: Api.js now correctly loads the Workout collection on the index page.

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,9 @@ const db = require('../models/fitness');
 
 module.exports = (app) => {
 
-    app.get('/all', (req, res) => {
-      db.workout.find({}, (err, data) => {
-          if (err) {
-              res.send(err);
-          } else {
-              res.json(data);
-          };
+    app.get('/api/workouts', (req, res) => {
+      db.find({}).then((workout) => {
+          res.send(workout)
       });
     });
 
